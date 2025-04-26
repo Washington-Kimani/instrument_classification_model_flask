15668b44ee77c24f0f1d6e4b659ec87a3b56daca
Washington Kimani <101510408+Washington-Kimani@users.noreply.github.com>
2025-04-26 23:55:19 +0300

    Some changes

--- app.py
+++ app.py
@@ -1,64 +1,8 @@
 from flask import Flask, render_template, request
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import load_model
-import numpy as np
 import os
 
 app = Flask(__name__)
 
-# load the model
-model = load_model(os.path.join('models', 'instrument_classification.h5'))
-
-# the array of instrument categories
-data_cat = ['Didgeridoo',
- 'Tambourine',
- 'Xylophone',
- 'acordian',
- 'alphorn',
- 'bagpipes',
- 'banjo',
- 'bongo drum',
- 'casaba',
- 'castanets',
- 'clarinet',
- 'clavichord',
- 'concertina',
- 'drums',
- 'dulcimer',
- 'flute',
- 'guiro',
- 'guitar',
- 'harmonica',
- 'harp',
- 'marakas',
- 'ocarina',
- 'piano',
- 'saxaphone',
- 'sitar',
- 'steel drum',
- 'trombone',
- 'trumpet',
- 'tuba',
- 'violin']
-
-# Image dimensions
-img_height = 224
-img_width = 224
-
-# prediction function
-def predict_label(image_path):
-    image_load = tf.keras.utils.load_img(image_path, target_size=(img_height, img_width))
-
-    # Preprocess the image
-    img_arr = tf.keras.utils.img_to_array(image_load)
-    img_bat = tf.expand_dims(img_arr, 0)
-
-    # Make prediction
-    predict = model.predict(img_bat)
-    score = tf.nn.softmax(predict)
-
-    return data_cat[np.argmax(score)]
 
 
 # home route render the index page
